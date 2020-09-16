ae4c415422b9052ce7f07177512a1d72b4ca8b16
CONROY Aedan <aconroy@AltranUK.local>
2020-09-16 16:43:33 +0100

    Moved some functionality to transactionHandler.py

--- supportbank.py
+++ supportbank.py
@@ -3,51 +3,24 @@ import sys
 import re
 import logging
 import json
-from datetime import datetime
-from datetime import timedelta
 import xml.etree.ElementTree as ET
+import transactionHandler
 
-csvfiles = ['Transactions2014.csv','DodgyTransactions2015.csv']
-transactions = []
-accounts = {}
-maxlengths = [0,0,0,0,0]
+files = ['Transactions2014.csv','DodgyTransactions2015.csv','Transactions2012.xml','Transactions2013.json']
 
 logging.basicConfig(filename='SupportBank.log',filemode='w',
 level=logging.DEBUG)
 
-class transaction:
-    def __init__(self,date,fromAccount,toAccount,narrative,amount):
-        self.date = date
-        self.fromAccount = fromAccount
-        self.toAccount = toAccount
-        self.narrative = narrative
-        self.amount = amount
+tHandler = transactionHandler.transactionHandler()
 
-def readRow(row):
-    #takes a list, example:['01/03/2015', 'Ben B', 'Sam N', 'Lunch', '3.80']
-    if re.fullmatch("(^[0-9]{2,4}[/-][0-9]{2}[/-][0-9]{2,4}$)|[0-9]+",row[0]) != None and \
-    re.fullmatch("^[0-9]+\.?[0-9]*$",row[4]) != None:
-        if re.fullmatch("(^[0-9]{2}/[0-9]{2}/[0-9]{4}$)",row[0]) != None:
-            date = datetime.strptime(row[0],"%d/%m/%Y")
-        elif re.fullmatch("(^[0-9]{4}-[0-9]{2}-[0-9]{2}$)",row[0]) != None:
-            date = datetime.strptime(row[0],"%Y-%m-%d")
-        elif re.fullmatch("(^[0-9]+$)",row[0]) != None:
-            #python epoch = 01/01/1970 xml file date based on excel epoch = 01/01/1900
-            date = datetime(1899,12,30) + timedelta(int(row[0]))
-
-        transactions.append(transaction(date,row[1],row[2],row[3],row[4]))
-
-        for i in range(len(row)):
-            if len(row[i]) > maxlengths[i]:
-                maxlengths[i] = len(row[i])
-        if row[1] in accounts:
-            accounts[row[1]] -= float(row[4])
-        else: accounts[row[1]] = 0 - float(row[4])
-        if row[2] in accounts:
-            accounts[row[2]] += float(row[4])
-        else: accounts[row[2]] = float(row[4])
-    else:
-        logging.info("Row ignored, invalid data:" + str(row))
+def importfile(path):
+    type = path[path.find(".")+1:]
+    if type == "json":
+        importjson(path)
+    elif type == "csv":
+        importcsv(path)
+    elif type == "xml":
+        importxml(path)
 
 def importjson(path):
     with open(path) as jsonfile:
@@ -58,9 +31,9 @@ def importjson(path):
             toAccount = str(dict["toAccount"])
             narrative = str(dict["narrative"])
             amount = str(dict["amount"])
-            readRow([date,fromAccount,toAccount,narrative,amount])
+            tHandler.readRow([date,fromAccount,toAccount,narrative,amount])
     logging.info("Imported file:" + str(path))
-    transactions.sort(key=sortKey)
+    tHandler.transactions.sort(key=sortKey)
 
 def importcsv(path):
     with open(path, newline='') as csvfile:
@@ -68,10 +41,10 @@ def importcsv(path):
         index = 0
         for row in csvreader:
             if index > 0:
-                readRow(row)
+                tHandler.readRow(row)
             index += 1
     logging.info("Imported file:" + str(path))
-    transactions.sort(key=sortKey)
+    tHandler.transactions.sort(key=sortKey)
 
 def importxml(path):
     tree = ET.parse(path)
@@ -83,68 +56,29 @@ def importxml(path):
         parties = transaction.find('Parties')
         fromAccount = parties.find('From').text
         toAccount = parties.find('To').text
-        readRow([date, fromAccount, toAccount ,description, value])
+        tHandler.readRow([date, fromAccount, toAccount ,description, value])
     logging.info("Imported file:" + str(path))
-    transactions.sort(key=sortKey)
+    tHandler.transactions.sort(key=sortKey)
 
 def sortKey(transaction):
     return transaction.date
 
 def main(argv):
-    for file in csvfiles:
-        importcsv(file)
+    for file in files:
+        importfile(file)
     response = ""
     print()
     while response != "Quit":
         print("Enter a command(List All/List[accountname]/Import filename/Quit):")
         response = input()
         if response == "List All":
-            string = "| " + "Account".ljust(maxlengths[1]) + " | " + \
-                "Balance".ljust(10) + "|"
-            print(string)
-            print("-"*len(string))
-            for i in accounts:
-                string = i.ljust(maxlengths[1]) + " | "
-                balance = str(accounts[i])
-                balance = balance[:balance.find(".")+3]
-                index = len(balance) - balance.find(".")
-                if index < 3:
-                    balance = balance.ljust(len(balance)+(3-index),'0')
-                print("| " + string + balance.rjust(10) + "|")
+            tHandler.listAll()
         elif re.fullmatch("List\[[A-Z][a-z]+ ?[A-Z]?\]",response) != None:
             account = response.split("[")[len(response.split("["))-1]
             account = account[:-1]
-            if account in accounts:
-                string = "| " + "Date".ljust(maxlengths[0]) + " | " + "From".ljust(maxlengths[1]) + \
-                    " | " + "To".ljust(maxlengths[2]) + " | " + "Narritave".ljust(maxlengths[3]) + \
-                    " | " + "Amount".ljust(maxlengths[4]) + "|"
-                print(string)
-                print("-"*len(string))
-                for i in transactions:
-                    if account==i.fromAccount or account==i.toAccount:
-                        string = "| "
-                        string = string + i.date.strftime("%d/%m/%Y").ljust(maxlengths[0]) + " | "
-                        string = string + i.fromAccount.ljust(maxlengths[1]) + " | "
-                        string = string + i.toAccount.ljust(maxlengths[2]) + " | "
-                        string = string + i.narrative.ljust(maxlengths[3]) + " | "
-                        balance = i.amount
-                        index = len(balance) - balance.find(".")
-                        if index < 3:
-                            balance = balance.ljust(len(balance)+(3-index),'0')
-                        string = string + balance.rjust(maxlengths[4]) + " | "
-                        print(string)
-            else:
-                print("Account not found")
-                logging.info("Account not found:" + "\"" + str(account)+ "\"")
+            tHandler.listAccount(account)
         elif response.split()[0] == "Import":
-            type = response.split()[1]
-            type = type[type.find(".")+1:]
-            if type == "json":
-                importjson(response.split()[1])
-            elif type == "csv":
-                importcsv(response.split()[1])
-            elif type == "xml":
-                importxml(response.split()[1])
+            importfile(response.split()[1])
         elif response != "Quit":
             print("invalid user response")
             logging.info("invalid user response:" + "\"" + str(response)+ "\"")
---
+++ transactionHandler.py
@@ -0,0 +1,82 @@
+import re
+import logging
+from datetime import datetime
+from datetime import timedelta
+
+class transaction:
+    def __init__(self,date,fromAccount,toAccount,narrative,amount):
+        self.date = date
+        self.fromAccount = fromAccount
+        self.toAccount = toAccount
+        self.narrative = narrative
+        self.amount = amount
+
+class transactionHandler:
+    def __init__(self):
+        self.transactions = []
+        self.accounts = {}
+        self.maxlengths = [0,0,0,0,0]
+
+    def listAll(self):
+        string = "| " + "Account".ljust(self.maxlengths[1]) + " | " + \
+            "Balance".ljust(10) + "|"
+        print(string)
+        print("-"*len(string))
+        for i in self.accounts:
+            string = i.ljust(self.maxlengths[1]) + " | "
+            balance = str(self.accounts[i])
+            balance = balance[:balance.find(".")+3]
+            index = len(balance) - balance.find(".")
+            if index < 3:
+                balance = balance.ljust(len(balance)+(3-index),'0')
+            print("| " + string + balance.rjust(10) + "|")
+
+    def listAccount(self,accountname):
+        if accountname in self.accounts:
+            string = "| " + "Date".ljust(self.maxlengths[0]) + " | " + "From".ljust(self.maxlengths[1]) + \
+                " | " + "To".ljust(self.maxlengths[2]) + " | " + "Narritave".ljust(self.maxlengths[3]) + \
+                " | " + "Amount".ljust(self.maxlengths[4]) + "|"
+            print(string)
+            print("-"*len(string))
+            for i in self.transactions:
+                if accountname==i.fromAccount or accountname==i.toAccount:
+                    string = "| "
+                    string = string + i.date.strftime("%d/%m/%Y").ljust(self.maxlengths[0]) + " | "
+                    string = string + i.fromAccount.ljust(self.maxlengths[1]) + " | "
+                    string = string + i.toAccount.ljust(self.maxlengths[2]) + " | "
+                    string = string + i.narrative.ljust(self.maxlengths[3]) + " | "
+                    balance = i.amount
+                    index = len(balance) - balance.find(".")
+                    if index < 3:
+                        balance = balance.ljust(len(balance)+(3-index),'0')
+                    string = string + balance.rjust(self.maxlengths[4]) + " | "
+                    print(string)
+        else:
+            print("Account not found")
+            logging.info("Account not found:" + "\"" + str(accountname)+ "\"")
+
+    def readRow(self,row):
+        #takes a list, example:['01/03/2015', 'Ben B', 'Sam N', 'Lunch', '3.80']
+        if re.fullmatch("(^[0-9]{2,4}[/-][0-9]{2}[/-][0-9]{2,4}$)|[0-9]+",row[0]) != None and \
+        re.fullmatch("^[0-9]+\.?[0-9]*$",row[4]) != None:
+            if re.fullmatch("(^[0-9]{2}/[0-9]{2}/[0-9]{4}$)",row[0]) != None:
+                date = datetime.strptime(row[0],"%d/%m/%Y")
+            elif re.fullmatch("(^[0-9]{4}-[0-9]{2}-[0-9]{2}$)",row[0]) != None:
+                date = datetime.strptime(row[0],"%Y-%m-%d")
+            elif re.fullmatch("(^[0-9]+$)",row[0]) != None:
+                #python epoch = 01/01/1970 xml file date based on excel epoch = 01/01/1900
+                date = datetime(1899,12,30) + timedelta(int(row[0]))
+
+            self.transactions.append(transaction(date,row[1],row[2],row[3],row[4]))
+
+            for i in range(len(row)):
+                if len(row[i]) > self.maxlengths[i]:
+                    self.maxlengths[i] = len(row[i])
+            if row[1] in self.accounts:
+                self.accounts[row[1]] -= float(row[4])
+            else: self.accounts[row[1]] = 0 - float(row[4])
+            if row[2] in self.accounts:
+                self.accounts[row[2]] += float(row[4])
+            else: self.accounts[row[2]] = float(row[4])
+        else:
+            logging.info("Row ignored, invalid data:" + str(row))
